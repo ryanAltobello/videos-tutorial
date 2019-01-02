f1418c79b2b3e694b4e04bf8ac5eb8ebbdcb1650
ryanAltobello <rchap504@gmail.com>
2019-01-02 13:19:48 -0600

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
@@ -15,7 +19,8 @@ class App extends React.Component {
     });
 
     this.setState({
-      videos: response.data.items
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
     });
   };
 
@@ -29,11 +34,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
